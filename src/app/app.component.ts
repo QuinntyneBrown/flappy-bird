cbf4a39a046c68e7fc18466e015183696101e1c1
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2017-11-02 07:15:48 -0400

    bird jump

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef } from "@angular/core";
 import { Bird } from "./models/bird";
 import { Tower } from "./models/tower";
 
@@ -8,36 +8,66 @@ import { Tower } from "./models/tower";
     selector: "app"
 })
 export class AppComponent {
+    constructor(public elementRef: ElementRef) {
+        this.handleClick = this.handleClick.bind(this);
+    }
+
     ngOnInit() {        
         for (let i = 0; i < 20; i++) {
             this.grid.push(new Array(30).fill('red'));
         }
 
+        this.towers.push(new Tower(false, 5, 3));
+        this.towers.push(new Tower(true, 8, 5));
+        this.towers.push(new Tower(false, 6, 7));
+        this.towers.push(new Tower(false, 5, 9));
+        this.towers.push(new Tower(true, 8, 24));
+        this.towers.push(new Tower(false, 6, 14));
+        this.towers.push(new Tower(false, 5, 17));
+        this.towers.push(new Tower(true, 8, 21));
+
+
         this.grid[this.bird.height][this.bird.position] = "yellow";
 
         this.timerId = setInterval(() => {
-            let towersCopy = this.towers.slice();
-
-            for (let i = 0; i < this.towers.length; i++) {
-
-                for (let j = 0; j < this.towers
-            }
-
-
             this.bird.height++;
-
+            let towers = this.towers.slice();
+            
             if (this.bird.height > 19 || this.bird.height < 0)
                 this.bird.height = 10;
-
+            
             let grid = [];
             for (let i = 0; i < 20; i++) { grid.push(new Array(30).fill('red')); }
 
+            for (let i = 0; i < towers.length; i++) {                
+                towers[i].position--;
+
+                if (towers[i].position < 0) {
+                    towers[i].position = 29;
+                    towers[i].height = Math.floor(Math.random() * 7) + 3;
+                }
+
+                for (let j = 0; j < towers[i].height; j++) {
+                    if (!towers[i].upright) {
+                        grid[j][towers[i].position] = "blue";
+                    } else {
+                        grid[19 - j][towers[i].position] = "blue";
+                    }
+                }
+            }
+            
             grid[this.bird.height][this.bird.position] = "yellow";
 
             this.grid = grid;
 
         }, 200);
     }
+
+    ngAfterViewInit() {
+        (<HTMLElement>this.elementRef.nativeElement).addEventListener("click", this.handleClick);
+    }
+
+    public handleClick(event$) { this.bird.height -= 3; }
 
     public grid: Array<any> = [];
 
--- src/app/models/tower.ts
+++ src/app/models/tower.ts
@@ -1,5 +1,7 @@
 ﻿export class Tower {
-    public direction: "topDown"| "bottomUp" = "topDown";
-    height: number = 0;
-    position: number = 0;
+    constructor(
+        public upright: boolean = false,
+        public height: number = 0,
+        public position: number = 0
+    ) { }
 }
